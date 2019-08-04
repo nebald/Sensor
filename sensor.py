0ab0eb76724354d818f26e8d1e055b3450f5fe60
Mark Bladen-Hovell <mark.hovell@envisics.com>
2019-08-04 12:58:21 +0100

    use short int ("H" array type not "i") to create 2 bytes per sensor value

--- sensor.py
+++ sensor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import array
+import os
+import sys
 
 class Sensor:
     name = "python"
@@ -11,7 +13,7 @@ class Sensor:
 
         sensor_size = self.columns * self.rows
         array_size = sensor_size * self.frames
-        chunk = array.array('i', (0 for i in range(array_size)))
+        chunk = array.array('H', (0 for i in range(array_size)))
 
         for j in range(0, self.frames):
             for i in range(0, sensor_size):
@@ -23,12 +25,21 @@ class Sensor:
 
         sensor_size = self.columns * self.rows
         array_size = sensor_size * self.frames
-        print_data = array.array('i', (0 for i in range(self.columns)))
+        print_data = array.array('H', (0 for i in range(self.columns)))
         for k in range(0, self.frames):
             for i in range(0, self.rows):
                 for j in range(0, self.columns):
                     print_data[j] = chunk[k * sensor_size + self.columns * i + j]
                 print(print_data)
+
+    def savefile(self, file_name, chunk):
+        try:
+            with open(file_name, "wb") as f:
+                f.write(chunk)
+                f.close()
+
+        except:
+            print("file save error")
 
 
 if __name__ == '__main__':
@@ -40,6 +51,7 @@ if __name__ == '__main__':
         ic.frames = 2
         chunk = ic.generate()
         ic.print(chunk)
+        ic.savefile('TestSave.bin', chunk)
 
     except:
         print("There's a problem with your code. Write better code!")
