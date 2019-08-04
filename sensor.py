17319cd4711a0a7ceaffc3b655c61577a0be1ce5
Mark Bladen-Hovell <mark.hovell@envisics.com>
2019-08-04 11:18:26 +0100

    add in class variables rows and columns add in print method

--- sensor.py
+++ sensor.py
@@ -2,25 +2,36 @@
 
 class Sensor:
     name = "python"
+    columns = 0
+    rows = 0
 
-    def generate_data(self, columns, rows, frames):
+    def generate(self, frames):
 
-        array_size = columns * rows
+        array_size = self.columns * self.rows
         chunk = bytearray(array_size)
 
         for i in range(0, array_size):
             chunk[i] = i
             i += 1
+        return chunk
 
-        print(chunk)
+    def print(self, chunk):
+
+        print_data = bytearray(self.columns)
+        for i in range(0, self.rows):
+            for j in range(0, self.columns):
+                print_data[j] = chunk[self.columns * i + j]
+            print(str(print_data))
 
 
 if __name__ == '__main__':
 
     try:
         ic = Sensor()
-
-        ic.generate_data(2, 2, 1)
+        ic.columns = 4
+        ic.rows = 4
+        chunk = ic.generate(1)
+        ic.print(chunk)
 
     except:
         print("There's a problem with your code. Write better code!")
